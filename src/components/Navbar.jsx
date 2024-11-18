6f14975383ce988acb04a25c88413c50ba2461d9
Avinash Kumar Gupta <robozealous@gmail.com>
2024-11-18 23:10:59 +0530

    initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <div>
+                <h3>Albums</h3>
+                    <ul>
+                        <li>Options</li>
+                        <li>Know more</li>
+                    </ul>
+            </div>
+
+
+        </>
+    )
+}
+
+export default Navbar
