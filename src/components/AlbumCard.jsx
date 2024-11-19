e659ae896a8aa5da8f8db5319f5ebd3cdaed49ab
Avinash Kumar Gupta <robozealous@gmail.com>
2024-11-19 15:11:44 +0530

    representation or display of the albums cards are almost to the mark, a little change required

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 const AlbumCard = ({ album }) => {
+
+    const randomNumberInRange = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
     return (
         <>
             <div className='w-full hover:scale-105 duration-200 cursor-pointer  rounded-md'>
-                <div className='albumCard p-3 flex flex-col justify-between items-center w-[70vw] sm:w-[320px] h-[70vw] sm:h-[320px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
-                    <div className='w-full'><h2 className='float-right'>{album.id}</h2></div>
+                <div style={{backgroundImage:`url(./src/assets/images/img${randomNumberInRange(1,13)}.png)`, backgroundSize: 'cover'}} className={`p-3 flex flex-col justify-between items-center w-[70vw] sm:w-[320px] h-[70vw] sm:h-[320px] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]`}>
+                    <div className='w-full'>
+                        <div className='rounded-full w-10 h-10 bg-white/70 float-right flex justify-center items-center'>
+                            <h2 className=' '>{album.id}</h2>
+                        </div>
+                    </div>
                     <div className='text-center w-full'><h2 className='text-white text-lg italic font-semibold'>{album.title}</h2></div>
                     <div className='flex justify-end gap-4 w-full bg-white/70'>
                         <button className='p-2 bg-blue-600 border-none hover:bg-blue-700 text-white rounded-md cursor-pointer'>Edit</button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
     const navigate = useNavigate();
     return (
         <>
-            <div className='shadow-[rgba(0,_0,_0,_0.25)_0px_15px_20px_-12px]'>
-                <div className='flex mx-auto w-full px-4 lg:w-8/12 py-4 justify-between items-center '>
+            <div className='  shadow-[rgba(0,_0,_0,_0.25)_0px_15px_20px_-12px]'>
+                <div className='text-gelasio flex mx-auto w-full px-4 lg:w-8/12 py-4 justify-between items-center '>
                     <div className='flex justify-center items-center relative'>
                         <FaBluesky className='text-3xl md:text-4xl mr-3 pt-1 text-black-700' />
                         <h3 onClick={() => navigate('/')} className='cursor-pointer mr-6 text-4xl md:text-5xl font-semibold text-purple-700 italic uppercase'>Albums</h3>
